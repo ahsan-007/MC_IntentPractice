1d83a0d3951e7e1193bc69ea80c757acf42c911e
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-11-06 13:56:40 +0500

    Added Logic
    * Created instances of UI components
    * Added onClickListener
    * Created an intent for dialer

--- app/src/main/java/com/example/mc_intentpractice/MainActivity.java
+++ app/src/main/java/com/example/mc_intentpractice/MainActivity.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
- public class MainActivity extends AppCompatActivity{
-    @Override
+ public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+     EditText phoneNo;
+     Button dial;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        phoneNo = findViewById(R.id.editTextPhone);
+        dial = findViewById(R.id.dial);
+        dial.setOnClickListener(this);
     }
+     @Override
+     public void onClick(View v) {
+         if(v.getId()==R.id.dial){
+             Uri uri = Uri.parse(("tel:"+phoneNo.getText().toString()));
+             Intent dialerIntent = new Intent(Intent.ACTION_DIAL,uri);
+             startActivity(dialerIntent);
+         }
+     }
+
  }
